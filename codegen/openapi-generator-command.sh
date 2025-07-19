ef526ff8e9241f327a1d2cf946e29c2285b0dddc
John Wang <johncwang@gmail.com>
2025-07-19 01:32:17 -0700

    enhance: `codegen`: add flexibility

--- codegen/openapi-generator-command.sh
+++ codegen/openapi-generator-command.sh
@@ -1,3 +1,3 @@
-java -jar openapi-generator-cli.jar generate -i swagger_spec.yaml -g go -o aha --package-name=aha
+java -jar openapi-generator-cli.jar generate -i openapi_spec.json -g go -o aha --package-name=aha --additional-properties=disallowAdditionalPropertiesIfNotPresent=false
 echo "\n\nfunc (apiClient *APIClient) HTTPClient() *http.Client { return apiClient.cfg.HTTPClient }" >> aha/client.go
 gofmt -s -w aha/*.go
